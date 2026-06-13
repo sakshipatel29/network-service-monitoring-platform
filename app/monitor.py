821b19982b2f7d78a6c35bfef2e8da4b78b0dc85
sakshipatel29 <sakshirpatel29062002@gmail.com>
2026-06-13 14:36:49 -0500

    Add email alert notification support

--- app/monitor.py
+++ app/monitor.py
@@ -6,7 +6,7 @@ import httpx
 from app.redis_client import redis_client
 from app.logging_config import logger
 from app.metrics import SERVICE_CHECK_TOTAL, SERVICE_UP, ALERT_TOTAL
-from app.notifications import send_slack_alert
+from app.notifications import send_slack_alert, send_email_alert
 
 def get_registered_services():
     stored_services = redis_client.lrange("services", 0, -1)
@@ -84,7 +84,7 @@ def check_single_service(service):
         )
 
         send_slack_alert(alert)
-
+        send_email_alert(alert)
 
 def check_all_services():
     services = get_registered_services()
--- app/notifications.py
+++ app/notifications.py
@@ -3,6 +3,8 @@ import httpx
 from dotenv import load_dotenv
 
 from app.logging_config import logger
+import smtplib
+from email.message import EmailMessage
 
 load_dotenv()
 
@@ -31,3 +33,57 @@ def send_slack_alert(alert: dict):
 
     except Exception as e:
         logger.error(f"Failed to send Slack alert: {e}")
+
+def send_email_alert(alert: dict):
+    email_enabled = os.getenv("EMAIL_ALERTS_ENABLED", "false").lower() == "true"
+
+    if not email_enabled:
+        logger.info("Email alerts are disabled. Skipping email notification.")
+        return
+
+    smtp_host = os.getenv("SMTP_HOST")
+    smtp_port = int(os.getenv("SMTP_PORT", 587))
+    smtp_username = os.getenv("SMTP_USERNAME")
+    smtp_password = os.getenv("SMTP_PASSWORD")
+    alert_email_from = os.getenv("ALERT_EMAIL_FROM")
+    alert_email_to = os.getenv("ALERT_EMAIL_TO")
+
+    required_values = [
+        smtp_host,
+        smtp_username,
+        smtp_password,
+        alert_email_from,
+        alert_email_to
+    ]
+
+    if not all(required_values):
+        logger.warning("Email alert configuration is incomplete. Skipping email notification.")
+        return
+
+    email = EmailMessage()
+    email["Subject"] = f"Service Alert: {alert['service_name']} is {alert['status']}"
+    email["From"] = alert_email_from
+    email["To"] = alert_email_to
+
+    email.set_content(
+        f"""
+Service Alert
+
+Service Name: {alert['service_name']}
+Status: {alert['status']}
+URL: {alert['url']}
+Time: {alert['created_at']}
+Message: {alert['message']}
+"""
+    )
+
+    try:
+        with smtplib.SMTP(smtp_host, smtp_port) as server:
+            server.starttls()
+            server.login(smtp_username, smtp_password)
+            server.send_message(email)
+
+        logger.info(f"Email alert sent for service={alert['service_name']}")
+
+    except Exception as e:
+        logger.error(f"Failed to send email alert: {e}")
